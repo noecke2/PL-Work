19a6b38098a3c3bdafd67886bf8b4a2d93cb7e3f
Andrew Noecker <noecke2@stolaf.edu>
2024-03-25 22:37:55 -0500

    Test github actions 2

--- 2024 Revamp/exec.R
+++ 2024 Revamp/exec.R
@@ -9,9 +9,15 @@ source("2024 Revamp/1_load_prereqs.R")
 # Load batter daily statistics from previous day
 source("2024 Revamp/batters_daily_stats.R")
 
-# Load pitcher daily statistics from previous dat
+# Load pitcher daily statistics from previous day
 source("2024 Revamp/pitcher_daily_stats.R")
 
 # Format table + send email
 source("2024 Revamp/send_email.R")
+
+batter_output %>%
+  write_rds("2024 Revamp/batter_output.rds")
+
+pitcher_output %>%
+  write_rds("2024 Revamp/pitcher_output.rds")
 
