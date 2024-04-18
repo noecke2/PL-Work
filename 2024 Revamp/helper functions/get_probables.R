7b883621b5034814bf5086b3f3a516437fc06af5
Andrew Noecker <noecke2@stolaf.edu>
2024-04-17 19:38:12 -0500

    Updated probables function

--- 2024 Revamp/helper functions/get_probables.R
+++ 2024 Revamp/helper functions/get_probables.R
@@ -3,7 +3,6 @@
 
 
 # Step 1: Pull game packs for the next day --------------------------------
-
 get_probables <- function(team = "Terminoeckers") {
   next_day_pks <- mlb_game_pks(Sys.Date()) %>% pull(game_pk)
   next_day_probables <- bind_rows(lapply(next_day_pks, mlb_probables))
@@ -12,12 +11,18 @@ get_probables <- function(team = "Terminoeckers") {
     filter(Team_Name == team) %>%
     pull(key_mlbam)
   
+  team_starter_mlb_ids <- long_pl_rosters %>%
+    filter(Team_Name == team, POS != "BN") %>%
+    pull(key_mlbam)
+  
   probables <- next_day_probables %>% 
     filter(id %in% team_mlb_ids) %>%
     left_join(next_day_probables, by = c("game_pk"), suffix = c("", "_opp")) %>%
     filter(id != id_opp | is.na(id_opp)) %>%
-    mutate(fullName_opp = ifelse(is.na(fullName_opp), "TBD", fullName_opp)) %>%
-    select(fullName, team, fullName_opp, team_opp)
+    mutate(fullName_opp = ifelse(is.na(fullName_opp), "TBD", fullName_opp),
+           is_starter = ifelse(id %in% team_starter_mlb_ids, 1, 0)) %>%
+    select(fullName, team, fullName_opp, team_opp, is_starter) %>%
+    arrange(-is_starter)
   
 
   if(nrow(probables) > 0){
@@ -40,12 +45,13 @@ get_probables <- function(team = "Terminoeckers") {
                           cell_text(weight = "bold"),
                           cell_fill(color = "#C9E9C3")),
                 locations = cells_body(
-                  columns = fullName
+                  columns = fullName,
+                  rows = is_starter == 1
                 )) %>%
       tab_style(style = list(
-                          cell_fill(color = "#F9E3D6"),
-                          cell_text(weight = "bold")),
-                locations = cells_body(columns = fullName_opp))
+                          cell_fill(color = "#F9E3D6")),
+                locations = cells_body(columns = fullName_opp)) %>%
+      cols_hide(is_starter)
     return(as_raw_html(table_gt))
     
   } else {
